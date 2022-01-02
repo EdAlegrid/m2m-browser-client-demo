624bf709d237ee8175629f7a9ba2d1264b72a24d
EdAlegrid <30911092+EdAlegrid@users.noreply.github.com>
2022-01-02 12:53:01 -0500

    Update device.js

--- device2/device.js
+++ device2/device.js
@@ -2,7 +2,7 @@ const { Device } = require('m2m');
 
 const device = new Device(200);
 
-device.connect('https://dev.node-m2m.com', () => {
+device.connect('https://www.node-m2m.com', () => {
   //device.setGpio({mode:'out', pin:[33, 35]}, gpio => console.log(gpio.pin, gpio.state)); // raspberry pi
   device.setGpio({mode:'out', pin:[33, 35], type:'simulation'}, gpio => console.log(gpio.pin, gpio.state)); // windows or linux
 });
