eea3b38aa262dd7506da403c66dce60f8f7636aa
EdAlegrid <30911092+EdAlegrid@users.noreply.github.com>
2022-01-02 12:52:16 -0500

    Add files via upload

--- device2/device.js
+++ device2/device.js
@@ -1,1 +1,8 @@
-test
+const { Device } = require('m2m');
+
+const device = new Device(200);
+
+device.connect('https://dev.node-m2m.com', () => {
+  //device.setGpio({mode:'out', pin:[33, 35]}, gpio => console.log(gpio.pin, gpio.state)); // raspberry pi
+  device.setGpio({mode:'out', pin:[33, 35], type:'simulation'}, gpio => console.log(gpio.pin, gpio.state)); // windows or linux
+});
