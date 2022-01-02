0d75c58fbbd110bc040e9f3c3a218546604ac04d
EdAlegrid <30911092+EdAlegrid@users.noreply.github.com>
2022-01-02 12:54:38 -0500

    Update device.js

--- device2/device.js
+++ device2/device.js
@@ -5,4 +5,13 @@ const device = new Device(200);
 device.connect('https://www.node-m2m.com', () => {
   //device.setGpio({mode:'out', pin:[33, 35]}, gpio => console.log(gpio.pin, gpio.state)); // raspberry pi
   device.setGpio({mode:'out', pin:[33, 35], type:'simulation'}, gpio => console.log(gpio.pin, gpio.state)); // windows or linux
+  
+  device.setData('random-number', (data) => {
+    let r = Math.floor(Math.random() * 100) + 25;
+    data.send(r);
+    console.log('random', r);
+  });
+
+  // error listener
+  device.on('error', (err) => console.log('error:', err));
 });
