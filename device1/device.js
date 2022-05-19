43aba830807c1afd3f37ade56d8560df91963674
EdAlegrid <30911092+EdAlegrid@users.noreply.github.com>
2022-05-19 08:37:52 -0400

    Add files via upload

--- device1/device.js
+++ device1/device.js
@@ -6,16 +6,16 @@ let myData = 'myData';
 
 device.connect('https://www.node-m2m.com', () => {
 
-  //device.setGpio({mode:'input', pin:[11, 13]}, (gpio) => console.log(gpio.pin, gpio.state) );
-  device.setGpio({mode:'input', pin:[11, 13], type:'simulation'}, (gpio) => console.log(gpio.pin, gpio.state) );
+  //device.setGpio({mode:'input', pin:[11, 13]}, (gpio) => console.log('input pin', gpio.pin, 'state', gpio.state)); // rpi
+  device.setGpio({mode:'input', pin:[11, 13], type:'simulation'}, (gpio) => console.log('input pin', gpio.pin, 'state', gpio.state));
   
-  //device.setGpio({mode:'output', pin:[33, 35]}); // raspberry pi
-  device.setGpio({mode:'output', pin:[33, 35], type:'simulation'}); // windows or linux
+  //device.setGpio({mode:'output', pin:[33, 35]}, (gpio) => console.log('output pin', gpio.pin, 'state', gpio.state)); // rpi
+  device.setGpio({mode:'output', pin:[33, 35], type:'simulation'}, (gpio) => console.log('output pin', gpio.pin, 'state', gpio.state));
   
   device.setData('get-data', (data) => {
     data.send(myData);
   });
-
+ 
   device.setData('send-data', (data) => {
     if(data.payload){
       myData = data.payload;
